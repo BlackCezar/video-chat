ee19db3f9f1ad37fbc2912de73893c8e15ec9a35
Doctor Maxin <30530749+BlackCezar@users.noreply.github.com>
2018-11-01 20:59:42 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
 var static = require('node-static');
 var http = require('http');
 var file = new(static.Server)();
+var port = process.env.PORT || 3000;
 var app = http.createServer(function (req, res) {
 	file.serve(req, res);
-}).listen(8080, ()=>{
-    console.log('OKK');
+}).listen(port, ()=>{
+    console.log('Server is running on ' + port);
 });
 
 var io = require('socket.io').listen(app);
